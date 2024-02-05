20381ddfa91ecd6eb0e68e3992c7eac1db50a402
messygr <46906387+messygr@users.noreply.github.com>
2024-02-05 17:21:32 +0000

    terraform test

---
+++ week_1/terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.14.0"
+    }
+  }
+}
+
+provider "google" {
+    project     = "zoomcamp-2024-413415"
+    region      = "europe-west6"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "week-1-terraform-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
