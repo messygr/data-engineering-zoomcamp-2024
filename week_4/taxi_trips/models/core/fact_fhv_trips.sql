07c90f9d2f137887e2046950d22b91f51c8970eb
messygr@gmail.com <messygr@gmail.com>
2024-02-24 00:15:37 +0000

    week 4 hw

--- week_4/taxi_trips/models/core/fact_fhv_trips.sql
+++ week_4/taxi_trips/models/core/fact_fhv_trips.sql
@@ -7,12 +7,12 @@
 with fhv_tripdata as (
     select *
     from {{ ref('stg_fhv_tripdata') }}
-)
+),
 
--- dim_zones as (
---     select * from {{ ref('dim_zones') }}
---     where borough != 'Unknown'
--- )
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
 select 
     fhv_tripdata.*
     -- pickup_zone.borough as pickup_borough, 
@@ -20,7 +20,7 @@ select
     -- dropoff_zone.borough as dropoff_borough, 
     -- dropoff_zone.zone as dropoff_zone
 from fhv_tripdata
--- inner join dim_zones as pickup_zone
--- on fhv_tripdata.pickup_locationid = pickup_zone.locationid
--- inner join dim_zones as dropoff_zone
--- on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
+inner join dim_zones as pickup_zone
+on fhv_tripdata.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
